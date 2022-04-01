351238893393dae6f83863c7656ded18eea89b31
Amberisfree <lavamber1996@gmail.com>
2022-04-01 19:26:34 +0800

    New updates for Heroku deployment

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-#x%uhqy(uom@f^ym_=(6$4ml!)mv$+&8_@3yi519ri966ac+f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
